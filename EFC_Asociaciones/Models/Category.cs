ca3213e58f4e1552c8db43277cfce62333ab735a
jacintic <jacintiglesias@gmail.com>
2022-11-02 10:26:55 +0100

    feature/ Category Class (without associations)

---
+++ EFC_Asociaciones/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFC_Asociaciones.Models;
+
+[Table("category")]
+public class Category
+{
+    [Key, Column("id",
+    Order = 0)]
+    public int Id { get; set; }
+
+    [Column("name",
+    TypeName = "varchar(100)",
+    Order = 1)]
+    public string Name { get; set; }
+
+    [Column("min_age",
+    TypeName = "int",
+    Order = 2)]
+    public int MinAge { get; set; }
+}

2cc5fc8e406885a9a542e425cd6ff9e43ee32218
jacintic <jacintiglesias@gmail.com>
2022-11-10 10:47:39 +0100

    feature/ angular buttons working with style from material and a simple script linked to a button

---
+++ angular_1/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // attr
+  title = 'angular_1';
+  // constructor
+  // methods
+  hello()
+  {
+    this.title = "Titulo cambiado desde un boton"
+  }
+}

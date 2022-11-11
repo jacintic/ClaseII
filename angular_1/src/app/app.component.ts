81e9587833fae97a92fb8f960b1527dca3605d6d
jacintic <jacintiglesias@gmail.com>
2022-11-11 10:16:11 +0100

    feature/ model-component-routing book-list

--- angular_1/src/app/app.component.ts
+++ angular_1/src/app/app.component.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
 
-export interface Book
-{
-  id: number;
-  title: string;
-  author: string;
-  price: number;
-}
 
 @Component({
   selector: 'app-root',
@@ -16,27 +9,6 @@ export interface Book
 export class AppComponent {
   // attr
   title: string = 'angular_1';
-  books: Book[] = [
-    {
-      id: 1,
-      title: "The Lord of the Rings",
-      author: "Tolkien",
-      price: 100
-    },
-    {
-      id: 2,
-      title: ".NET fundamentals",
-      author: "John Smith",
-      price: 54.30
-    },
-    {
-      id: 3,
-      title: "C# Definitive Guide",
-      author: "Paco Jhonson",
-      price: 35.50
-    }
-  ];
-  columnNames: string[] = ["id", "title", "author", "price", "actions"];
   // constructor
   // methods
   hello()
--- angular_1/src/app/book-list/book-list.component.ts
+++ angular_1/src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../models/book.model';
 
 @Component({
   selector: 'app-book-list',
@@ -6,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
+
+  books: Book[] = [
+    {
+      id: 1,
+      title: "The Lord of the Rings",
+      author: "Tolkien",
+      price: 100
+    },
+    {
+      id: 2,
+      title: ".NET fundamentals",
+      author: "John Smith",
+      price: 54.30
+    },
+    {
+      id: 3,
+      title: "C# Definitive Guide",
+      author: "Paco Jhonson",
+      price: 35.50
+    }
+  ];
+  columnNames: string[] = ["id", "title", "author", "price", "actions"];
 
   constructor() { }
 

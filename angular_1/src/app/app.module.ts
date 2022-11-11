990efe531df6df7866074f9cbc35cb53247a435c
jacintic <jacintiglesias@gmail.com>
2022-11-11 10:04:32 +0100

    feature/ routing sketch

--- angular_1/src/app/app.module.ts
+++ angular_1/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { BookListComponent } from './book-list/book-list.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule,
     MatButtonModule,
     MatTableModule,
-    MatIconModule
+    MatIconModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'book-list', pathMatch: 'full'},
+      { path: 'book-list', component: BookListComponent },
+      { path: 'book-detail', component: BookDetailComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
